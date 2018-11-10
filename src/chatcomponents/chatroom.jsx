87d9b5af0a08bc6ccc8e5f6d2557945875bc0abd
HarringtonR <ross.ludeke@gmail.com>
2018-11-10 14:04:19 -0500

    chatkit test

--- src/chatcomponents/chatroom.jsx
+++ src/chatcomponents/chatroom.jsx
@@ -30,7 +30,7 @@ export default class Chatroom extends Component {
         currentUser: currentUser
          })
         return currentUser.subscribeToRoom({
-          roomId: 19225978,
+          roomId: 19266444,
           messageLimit: 50,
           hooks: {
             onNewMessage: message => {
