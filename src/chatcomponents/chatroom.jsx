8cb22127152f8919ba8c1edd328154becd2ffdec
HarringtonR <ross.ludeke@gmail.com>
2018-11-10 21:13:21 -0500

    changed room

--- src/chatcomponents/chatroom.jsx
+++ src/chatcomponents/chatroom.jsx
@@ -30,7 +30,7 @@ export default class Chatroom extends Component {
         currentUser: currentUser
          })
         return currentUser.subscribeToRoom({
-          roomId: 19266444,
+          roomId: 19373346,
           messageLimit: 50,
           hooks: {
             onNewMessage: message => {
